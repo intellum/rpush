ff79335f75f22d6bbc852e4a822942b21e58612a
Raimon Malatesta <graimon@gmail.com>
2024-07-17 14:05:05 -0500

    skip android notification payload

--- lib/rpush/client/active_model/fcm/notification.rb
+++ lib/rpush/client/active_model/fcm/notification.rb
@@ -86,6 +86,9 @@ module Rpush
           end
 
           def android_notification
+            return {}
+            # currently our Android App is not able to deserialize this data
+            # if we omit it, the notification will display, otherwise it will not
             json = notification&.slice(*ANDROID_NOTIFICATION_KEYS) || {}
             json['notification_priority'] = priority_for_notification if priority
             json['sound'] = sound if sound
